a87d6a106f2be3d22ed9186b51f2a942613e29a1
PiBot <PiBot@email.com>
2022-07-14 15:33:31 +0000

    Publish accurate lidar data

--- hwr_indoor_navigation/publisher_lidar_data.py
+++ hwr_indoor_navigation/publisher_lidar_data.py
@@ -17,55 +17,54 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.lidar = lidar.Lidar()
-        self.publisher_ = self.create_publisher(LaserScan, 'topic', 10)
+        self.publisher_ = self.create_publisher(LaserScan, 'lidar_topic', 10)
 
     def publish(self):
-        for entry in self.lidar.capture_output():
-            scan_data = LaserScan()
-            scan_data.header.stamp = self.get_clock().now().to_msg()
-            scan_data.header.frame_id = "laser_frame"
-            scan_data.angle_min = entry.angle * math.pi / 180
-            scan_data.angle_max = entry.angle * math.pi / 180
-            scan_data.angle_increment = 0.0
-            scan_data.time_increment = 0.0
-            scan_data.scan_time = 0.0
-            scan_data.range_min = entry.dist
-            scan_data.range_max = entry.dist
-            scan_data.ranges = [entry.dist]
-            scan_data.intensities = []
-            self.publisher_.publish(scan_data)
-            self.get_logger().info(f"Publishing: {scan_data}")
-
-    def fake_publish(self):
-        n_readings = 100
-        freq = 40
-        count = 0
+        output = self.lidar.capture_output()
         while True:
-            ranges = [5.0] * n_readings
-            intensities = [float(100 + count)] * n_readings
-            scan_data = LaserScan()
-            scan_data.header.stamp = self.get_clock().now().to_msg()
-            scan_data.header.frame_id = "laser_frame"
-            scan_data.angle_min = -1.57
-            scan_data.angle_max = 1.57
-            scan_data.angle_increment = 3.14 / n_readings
-            scan_data.time_increment = (1 / freq) / n_readings
-            scan_data.scan_time = 1 / freq
-            scan_data.range_min = 0.0
-            scan_data.range_max = 100.0
-            scan_data.ranges = ranges
-            scan_data.intensities = intensities
-            self.publisher_.publish(scan_data)
-            self.get_logger().info(f"Publishing: {scan_data}")
+            rot_start = time.time()
 
+            ranges = []
+            intensities = []
+            n_readings = 0
+            while True:
+                entry = next(output)
+
+                if entry.angle == 0:
+                    rot_end = time.time()
+                    rot_duration = rot_end - rot_start
+
+                    scan_data = LaserScan()
+                    scan_data.header.stamp = self.get_clock().now().to_msg()
+                    scan_data.header.frame_id = "laser_frame"
+                    scan_data.angle_min = 0.0
+                    scan_data.angle_max = 2 * math.pi
+                    scan_data.angle_increment = 2 * math.pi / n_readings
+                    scan_data.time_increment = rot_duration / n_readings
+                    scan_data.scan_time = rot_duration
+                    scan_data.range_min = 0.0
+                    scan_data.range_max = 10.0
+                    scan_data.ranges = ranges
+                    scan_data.intensities = intensities
+                    self.publisher_.publish(scan_data)
+                    self.get_logger().info(f"Publishing: {scan_data}")
+
+                    ranges.clear()
+                    intensities.clear()
+                    n_readings = 0
+                    break
+
+                ranges.append(float(entry.dist))
+                intensities.append(float(entry.angle))
+
+                n_readings += 1
 
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_publisher = MinimalPublisher()
 
-    # minimal_publisher.publish()
-    minimal_publisher.fake_publish()
+    minimal_publisher.publish()
 
     rclpy.spin(minimal_publisher)
 
