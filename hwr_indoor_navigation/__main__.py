306f6e935deed9f9e96f6431e1382ae16e06048b
0scarB <oscarb@protonmail.com>
2022-07-28 14:40:09 +0200

    Start app from main function

--- hwr_indoor_navigation/__main__.py
+++ hwr_indoor_navigation/__main__.py
@@ -1,33 +1,5 @@
-import logging
-import os
-import sys
-
-import event
-import robot
-import terminal_controls
-from unit import UnitValue
-
-sys.path.append(os.path.dirname(__file__))
+from . import main
 
 
-import app
-
-
-app = app.App(app.Config(
-    event=event.Config(
-        log_level=logging.DEBUG
-    ),
-    services=[
-        ("robot", robot.Config(use_lidar=True)),
-        ("terminal_controls", terminal_controls.Config(
-            movement_speed=UnitValue(100, "forward_backward_motor_pwm_duty_cycle"),
-            steering_increment=UnitValue(20, "steering_motor_pwm_frequency"),
-            forward_key="w",
-            backward_key="s",
-            left_key="a",
-            right_key="d",
-        ))
-    ]
-))
-
-app.start()
+if __name__ == "__main__":
+    main()
