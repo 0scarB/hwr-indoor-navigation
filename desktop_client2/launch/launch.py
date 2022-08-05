d58f0927d2fb7e7c9604de9e60d4aee6d805cbed
0scarB <oscarb@protonmail.com>
2022-08-05 04:05:48 +0200

    Add map

--- desktop_client2/launch/launch.py
+++ desktop_client2/launch/launch.py
@@ -4,43 +4,62 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='desktop_client2').find('desktop_client2')
-    default_model_path = os.path.abspath(os.path.join(pkg_share, 'src/description/pi_bot_description.urdf'))
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="desktop_client2").find("desktop_client2")
+    default_model_path = os.path.join(pkg_share, "src/description/pi_bot_description.urdf")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
+    bringup_dir = launch_ros.substitutions.FindPackageShare(package="desktop_client2").find("nav2_bringup")
+    bringup_launch_file = os.path.join(bringup_dir, 'launch', 'navigation_launch.py')
+    slam_toolbox_dir = launch_ros.substitutions.FindPackageShare(package="desktop_client2").find("slam_toolbox")
+    slam_launch_file = os.path.join(slam_toolbox_dir, "launch", "online_async_launch.py")
 
     robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": Command(["xacro ", LaunchConfiguration("model")])}]
     )
     joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
     )
     joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        condition=launch.conditions.IfCondition(LaunchConfiguration("gui"))
     )
     rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(
+            name="gui",
+            default_value="True",
+            description="Flag to enable joint_state_publisher_gui"
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name="model",
+            default_value=default_model_path,
+            description="Absolute path to robot urdf file"
+        ),
+        launch.actions.DeclareLaunchArgument(
+            name="rvizconfig",
+            default_value=default_rviz_config_path,
+            description="Absolute path to rviz config file"
+        ),
+        launch.actions.IncludeLaunchDescription(
+            launch.launch_description_sources.PythonLaunchDescriptionSource([bringup_launch_file])
+        ),
+        launch.actions.IncludeLaunchDescription(
+            launch.launch_description_sources.PythonLaunchDescriptionSource([slam_launch_file])
+        ),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node
+        rviz_node,
     ])
